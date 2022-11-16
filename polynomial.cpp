ca4ab954a34b039e41c107e6cb9de8a0a42df253
madara1319 <mateusz.bik@op.pl>
2022-11-16 21:12:37 +0100

    Did define classes and setters and getters but still some work needs to be done

--- polynomial.cpp
+++ polynomial.cpp
@@ -1,6 +1,61 @@
 #include <iostream>
+#include <math.h>
+
+using std::cout, std::cin, std::string, std::endl;
+
+class polynomial
+{
+	private:
+		double a;
+		double b;
+		double c;
+
+
+	public:
+	void seta(double A)
+	{
+		a=A;
+	};
+	void setb(double B)
+	{
+		b=B;
+	};
+	void setc(double C)
+	{
+		c=C;
+	};
+	double geta()
+	{
+		return a;
+	};
+	double getb()
+	{
+		return b;
+	};
+	double getc()
+	{
+		return c;
+	};
+	
+
+	void checkEquation()
+	{
+		if ((pow(b,2.0)-4.0*a*c)>=0)
+			cout<<"Theres or there are roots for this coefficients";
+		else
+			cout<<"Theres no root for such coefficients";
+
+	};
+};
+
 
 int main()
 {
-	std::cout<<"Hello world!";
+	polynomial equation1=polynomial();
+
+	equation1.seta(3);
+	equation1.setb(6);
+	equation1.setc(2);
+	cout<<"Equation looks like y = "<< equation1.geta() <<" * x ^2 + "<<equation1.getb()<< " * x + " <<equation1.getc()<<endl;
+	equation1.checkEquation();
 }
