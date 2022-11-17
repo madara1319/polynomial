f791498218221ddd91d539db9a49f1d752031ad4
madara1319 <mateusz.bik@op.pl>
2022-11-17 20:01:25 +0100

    Task seems done

--- polynomial.cpp
+++ polynomial.cpp
@@ -2,6 +2,7 @@
 #include <math.h>
 
 using std::cout, std::cin, std::string, std::endl;
+//__________________________________________________________________________________
 
 class polynomial
 {
@@ -9,9 +10,13 @@ class polynomial
 		double a;
 		double b;
 		double c;
+		double x1;
+		double x2;
 
 
 	public:
+//__________________________________________________________________________________
+
 	void seta(double A)
 	{
 		a=A;
@@ -37,25 +42,69 @@ class polynomial
 		return c;
 	};
 	
-
-	void checkEquation()
+//__________________________________________________________________________________
+	bool checkEquation()
 	{
 		if ((pow(b,2.0)-4.0*a*c)>=0)
-			cout<<"Theres or there are roots for this coefficients";
+		{
+			cout<<"Theres or there are roots for this coefficients"<<endl;
+			return 1;
+		}
 		else
-			cout<<"Theres no root for such coefficients";
+		{
+			cout<<"Theres no root for such coefficients"<<endl;
+			return 0;
+		}
 
 	};
+	
+	void rootsFinder()
+	{
+		if (checkEquation()==true)
+		{
+			x1=(-b-sqrt((pow(b,2.0)-4.0*a*c)))/(2*a);
+			x2=(-b+sqrt((pow(b,2.0)-4.0*a*c)))/(2*a);
+
+			cout<<"Root number one x1 equals = "<<x1<<endl;
+			cout<<"Root number two x2 equals = "<<x2<<endl;
+		}
+		else
+			cout<<"Cannot compute the roots"<<endl;
+	}
+
+//__________________________________________________________________________________
+
+//Constructor for class polynomial
+
+	polynomial(double A, double B, double C)
+	{
+		a=A;
+		b=B;
+		c=C;
+	};
+		
 };
 
+//__________________________________________________________________________________
 
 int main()
-{
-	polynomial equation1=polynomial();
-
-	equation1.seta(3);
-	equation1.setb(6);
-	equation1.setc(2);
-	cout<<"Equation looks like y = "<< equation1.geta() <<" * x ^2 + "<<equation1.getb()<< " * x + " <<equation1.getc()<<endl;
-	equation1.checkEquation();
+{	
+	double coefa {};
+	double coefb {};
+	double coefc {};
+	polynomial equation1=polynomial(coefa,coefb,coefc);
+	cout <<"Welcome to second-order polynomial roots finder"<<endl;
+	cout<<"Enter first coefficient for x^2 -> a "<<endl;
+	cin>>coefa;
+	equation1.seta(coefa);
+	cout<<"Enter second coefficient for x -> b "<<endl;
+	cin>>coefb;
+	equation1.setb(coefb);
+	cout<<"Enter free coefficient c "<<endl;
+	cin>>coefc;
+	equation1.setc(coefc);
+	
+	cout<<"The equation is y = "<< equation1.geta() <<" * x ^2 + "<<equation1.getb()<< " * x + " <<equation1.getc()<<endl;
+//	equation1.checkEquation();
+	equation1.rootsFinder();
 }
